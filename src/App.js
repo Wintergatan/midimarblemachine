8872d061f6ed90c7a5266d8d3a3a6b15bb2abb9e
Sebastiaan Jansen <whitebird@live.Be>
2018-02-14 21:59:02 +0100

    trigger precommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">midi marble machine</p>
       </div>
     );
   }
